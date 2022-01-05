7a36effec208f8ecae85099d4642899012a70d30
Tymothy Lonero <thlonero@gmail.com>
2022-01-05 10:34:24 -0700

    Initial commit

---
+++ src/main/java/com/cognixia/jumplus/DollarsBankSpringbootAppV1Application.java
@@ -0,0 +1,13 @@
+package com.cognixia.jumplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarsBankSpringbootAppV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollarsBankSpringbootAppV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognixia/jumplus/DollarsBankSpringbootAppV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognixia.jumplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollarsBankSpringbootAppV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
